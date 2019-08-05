e2be73d2603a5f52c2ef8adaf44c3afc61aab815
Mohamad D Abdulkarim <mohamaddabdulkarim@MacbookProDia.local>
2019-08-05 19:17:34 -0400

    ch3

---
+++ users/test.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+
+class CustomerUserTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username = 'dill',
+            email = 'dia@gmail.com',
+            password = 'testpass123'
+        )
+        self.assertEqual(user.username, 'dill')
+        self.assertEqual(user.email, 'dia@gmail.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        user = User.objects.create_superuser(
+            username = 'superadmin',
+            email = 'super@email.com',
+            password = 'super@1234'
+        )
+        self.assertEqual(user.username, 'superadmin')
+        self.assertEqual(user.email, 'super@email.com')
+        self.assertTrue(user.is_active)
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
